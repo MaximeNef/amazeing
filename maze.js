2f64dd06e273e0153bffb94283d1c9fa2aceb4a9
MaximeNef <maximenef1@gmail.com>
2020-11-23 16:07:14 +0100

    laby 1 fini

--- maze.js
+++ maze.js
@@ -1,14 +1,31 @@
 const body = document.querySelector("body");
 body.className = "body" ;
-
+body.style.width = "520px" ;
 const multiline = 
 `***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********`
+
+
+
+/* body.style.width = "920px" ;
+const multiline = 
+`**********************
+*..S.................*
+********************.*
+*....................*
+*.********************
+*...................T*` */
+
+
+
+
+
+
 
 
 for(let i = 0 ; i < multiline.length ; i++){
     const ligne = document.createElement("div")
     ligne.className =  "coco"
-    ligne.innerHTML = multiline[i].split('')
+    //ligne.innerHTML = multiline[i].split('')
     body.appendChild(ligne)
 
     if(multiline[i] === "*"){
@@ -17,11 +34,51 @@ for(let i = 0 ; i < multiline.length ; i++){
     if(multiline[i]=== "."){
         ligne.className = "chemin"
     }
-    if(multiline[i]=== "S"){
-        let perso = document.createElement("div")
-        perso.className = "perso"
-        ligne.appendChild(perso)
-    }
-
+    
+ 
+   
 }
 
+const balle  = document.createElement('div')
+balle.className = "perso"
+document.querySelector("body > div:nth-child(16)").appendChild(balle)
+
+
+let posX = 16;
+
+document.body.addEventListener("keydown", function(e){
+    if(e.code === "ArrowRight"){
+        
+       if(document.querySelector("body > div:nth-child("+(posX+1)+")").classList.contains('mur')) {console.log('hiit the wall')}
+    else{
+        posX++
+        document.querySelector("body > div:nth-child("+posX+")").appendChild(balle)
+        console.log(e.code)}
+    }
+
+
+    if(e.code === "ArrowLeft"){
+        if(document.querySelector("body > div:nth-child("+(posX-1)+")").classList.contains('mur')) {console.log('hiit the wall')}
+       else{ posX--
+        document.querySelector("body > div:nth-child("+posX+")").appendChild(balle)
+        console.log(e.code)}
+    }
+
+
+    if(e.code === "ArrowUp"){
+        if(document.querySelector("body > div:nth-child("+(posX-13)+")").classList.contains('mur')) {console.log('hiit the wall')}
+        else{
+        posX-=13
+        document.querySelector("body > div:nth-child("+posX+")").appendChild(balle)
+        console.log(e.code)}
+    }
+
+
+    if(e.code === "ArrowDown"){
+        if(document.querySelector("body > div:nth-child("+(posX+13)+")").classList.contains('mur')) {console.log('hiit the wall')}
+        else{
+        posX+=13
+        document.querySelector("body > div:nth-child("+posX+")").appendChild(balle)
+        console.log(e.code)}
+    }
+})
